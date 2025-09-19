632e10dd9ea045d64887f3977a5d69769c4137f7
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-09-19 20:33:27 +1000

    feat(api): add CORS support and configure in-memory Todo repository

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -6,6 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("devCors", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    })
+})
+builder.Services.AddSingleton<backend.Services.ITodoRepository, backend.Services.InMemoryTodoRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("DevCors");
 
 app.UseAuthorization();
 
